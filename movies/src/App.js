dc9be293c7488c5762dc3744108e1170f7db2523
Jacob Erlandsson <erlandssonjacob@hotmail.com>
2022-12-03 22:12:03 +0100

    images now loading

--- movies/src/App.js
+++ movies/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Movie from "./components/Movie";
 
 const movies_from_api =
-  "https://api.themoviedb.org/3/movie/top_rated?api_key=1026eea38d091b7fb22916e8c7542406&language=en-US&page=1";
+  "https://api.themoviedb.org/3/movie/popular?api_key=1026eea38d091b7fb22916e8c7542406&language=en-US&page=1";
 
 function App() {
   const [movies, setMovies] = useState([]);
--- movies/src/components/Movie.js
+++ movies/src/components/Movie.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-const movieImages =
-  "https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=1026eea38d091b7fb22916e8c7542406&language=en-US";
+const movieImages = "https://www.themoviedb.org/t/p/w440_and_h660_face";
 
-function Movie({ title, poster_path, overview, vote_average }) {
+function Movie({ title, backdrop_path, overview, vote_average }) {
   return (
     <div className="movie">
-      <img src={movieImages + poster_path} alt={title} />
+      <img src={movieImages + backdrop_path} alt={title} />
     </div>
   );
 }
