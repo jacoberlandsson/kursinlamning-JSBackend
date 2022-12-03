96677259218c261212048c7eeded262e9f959af9
Jacob Erlandsson <erlandssonjacob@hotmail.com>
2022-12-03 20:58:29 +0100

    123

--- movies/src/App.js
+++ movies/src/App.js
@@ -17,6 +17,11 @@ function App() {
       });
   }, []);
 
-  return <div>{movies.length > 0 && movies.map((movie) => <Movie />)}</div>;
+  return (
+    <div>
+      {movies.length > 0 &&
+        movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+    </div>
+  );
 }
 export default App;
--- movies/src/components/Movie.js
+++ movies/src/components/Movie.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-function Movie() {
-  return <div className="movie">This is a movie component</div>;
+const movieImages =
+  "https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=1026eea38d091b7fb22916e8c7542406&language=en-US";
+
+function Movie({ title, poster_path, overview, vote_average }) {
+  return (
+    <div className="movie">
+      <img src={movieImages + poster_path} alt={title} />
+    </div>
+  );
 }
 
 export default Movie;
